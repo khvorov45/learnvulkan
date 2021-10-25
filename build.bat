ce4a087521fa5457a914170bfe85b98b77896871
Arseniy Khvorov <khvorov45@gmail.com>
2021-10-25 13:14:31 +1100

    add shaders

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 pushd build
+glslc ../code/shader.vert -o vert.spv
+glslc ../code/shader.frag -o frag.spv
 cl -Od -Z7 -nologo -TC -I%VK_SDK_PATH%/include ../code/main.c -link -LIBPATH:%VK_SDK_PATH%/Lib User32.lib Gdi32.lib vulkan-1.lib
 popd
 echo done
